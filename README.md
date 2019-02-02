8c9d2e9cdcb30fef68a0aec38876cd3900a9ab6a
Moussa mokhtari <m_mokhtari@esi.dz>
2019-02-02 22:27:04 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,16 @@
+
+
+# Image viewer
+
+```
+$ ionic cordova plugin add com-poolgazal-imageviewer
+$ npm install @ionic-native/image-viewer
+```
+
+Plugin Repo: [https://github.com/poolgazal/com-poolgazal-imageviewer](https://github.com/poolgazal/com-poolgazal-imageviewer)
+
+AppMinimize is a plugin to minimize the application on android devices
+
+## Supported platforms
+- Android
+-IOS
---
+++ index.d.ts
@@ -0,0 +1,34 @@
+import { IonicNativePlugin } from '@ionic-native/core';
+export interface ImageViewerOptions {
+    /**
+     * Set to false to disable the share button (Android only). Default: true
+     */
+    share?: boolean;
+}
+/**
+ * @name Image Viewer
+ * @description This plugin can display your image in full screen with the ability to pan, zoom, and share the image.
+ * @usage
+ * ```typescript
+ * import { ImageViewer } from '@ionic-native/image-viewer/ngx';
+ *
+ * constructor(private imageViewer: ImageViewer) { }
+ *
+ * ...
+ *
+ * this.imageViewer.show('https://mysite.com/path/to/image.jpg');
+ *
+ * this.imageViewer.show('https://mysite.com/path/to/image.jpg', 'My image title', {share: false});
+ * ```
+ */
+export declare class ImageViewerOriginal extends IonicNativePlugin {
+    /**
+     * Shows an image in full screen
+     * @param url {string} URL or path to image
+     * @param title {string}
+     * @param options {ImageViewerOptions}
+     */
+    show(url: string, title?: string, options?: ImageViewerOptions): void;
+}
+
+export declare const ImageViewer: ImageViewerOriginal;
